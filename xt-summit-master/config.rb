3331651cc99aa7fe7064e49b3288b8f8e16059e0
Manjari <mprithani@sapient.com>
2019-05-09 20:08:33 +0530

    Initial commit

---
+++ xt-summit-master/config.rb
@@ -0,0 +1,20 @@
+# remove this file if you have global paths setup for compass and ruby
+http_path = "/"
+css_dir = "public/css/"
+sass_dir = "resources/scss"
+images_dir = "public/images/"
+fonts_dir = "public/fonts"
+javascripts_dir = "public/js"
+add_import_path "node_modules/foundation-sites/scss"
+add_import_path "node_modules/font-awesome/scss"
+
+sourcemap = true
+
+#output_style = :expanded
+output_style = :compressed
+environment = :development
+
+
+relative_assets = true
+line_comments = false
+
